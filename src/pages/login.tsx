e46f987e611e7bad11b8aba29e6382014d3b7125
levifernandes00 <levi.o.fernandes@outlook.com>
2023-03-01 11:13:02 +0100

    fix: changed from form to div

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -44,7 +44,7 @@ const MainPage: React.FC = () => {
                 <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                     Sign in to your account
                 </h1>
-                <form className="space-y-4 md:space-y-6">
+                <div className="space-y-4 md:space-y-6">
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
                         <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border sm:text-sm rounded-lg  block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@company.com" />
@@ -54,7 +54,7 @@ const MainPage: React.FC = () => {
                         <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••••" className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" />
                     </div>
                     <button type="submit" onClick={signin} className="w-full text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Sign in</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
